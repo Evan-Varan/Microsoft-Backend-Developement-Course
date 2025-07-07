8832c15514ee06930af7854c383cf63f223ff302
Evan Varan <evan.varan@gmail.com>
2025-07-07 18:39:32 -0500

    Async Examples

---
+++ Introduction to C#/AsyncronousExample/ExampleAsyncMethods.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+class DelayedWorker
+{
+    public async Task SimulateDelay()
+    {
+        await Task.Delay(5000); //waits for 5 seconds then prints
+        Console.WriteLine("operation completed after 5 seconds.");
+    }
+}
+
+class DelayedFileReader
+{
+    public async Task ReadFileAsync(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string content = await
+            reader.ReadFileAsync("example.txt");
+        }
+        Console.WriteLine(content);
+    }
+}
+
+class APIReader
+{
+    public async Task GetDataFromMultipleSources()
+    {
+        Task<string> data1 = GetDataFromApiAsync("https://api.example.com/data1");
+        Task<string> data2 = GetDataFromApiAsync("https://api.example.com/data1"); //Dummy Async Methods
+        await Task.WhenAll(data1, data2);
+        Console.WriteLine(data1.Result);
+        Console.WriteLine(data2.Result);
+    }
+}
---
+++ Introduction to C#/AsyncronousExample/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+/* Note this code is sudocode and will not build properly */
+class Program
+{
+    static async Task Main() // Main must be async to call an async method
+    {
+        Console.WriteLine("Hello, World!");
+
+        /*Example One */
+        var worker = new DelayedWorker(); // Worker instantiation
+        await worker.SimulateDelay(); //Call the delay method
+
+        /*Example Two */
+        var fileReader = new DelayedFileReader();
+        await fileReader.ReadFileAsync();
+
+        /*Example Three (Multiple Async Calls) */
+        var apiReader = new APIReader();
+        await APIReader.GetDataFromMultipleSources();
+    }
+}
+
+
